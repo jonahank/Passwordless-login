60f30c976662f0b62fb5ab3f349c9b5999d1974d
Jonathan Kristensen <jonahank7@gmail.com>
2021-08-08 23:27:43 +0200

    Decryption works with encmsg.txt

---
+++ Decryptz.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.InputStreamReader;
+
+public class Decryptz {
+
+    public static boolean decrypt() throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        
+        System.out.println("   ***********************************************************   ");
+        System.out.println("   *****                                                 *****   ");
+        System.out.println("   *****       Update encrypted_message.txt with OTP     *****   ");
+        System.out.println("   *****   ___________________________________________   *****   ");
+        System.out.println("   *****                                                 *****   ");
+        System.out.println("   *****             (press enter when done)             *****   ");
+        String command = "";
+        command = sc.nextLine();
+
+
+        //User check:
+        File OTP = new File("encmsg.txt");
+        Scanner OTPReader = new Scanner(OTP);
+        while (OTPReader.hasNext()) {
+            System.out.println(OTPReader.nextLine());
+        }
+        System.out.println();
+        System.out.println("   *****            Is this your message (Y/N)?          *****   ");
+        OTPReader.close();
+        command = sc.nextLine();
+
+        // If correct, perform GPG commands in Unix Linux commandline and output the OTP to the user.
+        if (command.toUpperCase().equals("Y")){
+            
+            // perform decryption
+            // processBuilder.command("bash", "-c", encryptedMsg);
+            System.out.println("   *****                    OTP BELOW                    *****   ");
+            System.out.println("   *****   -------------------------------------------   *****   ");
+            System.out.println("   *****                                                 *****   ");
+        
+            processBuilder.command("bash", "-c", "gpg --decrypt encmsg.txt");
+    
+            //Boiler plate initialization
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    
+            //Creating output string
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+    
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("                     " + output);
+                System.out.println("   *****                                                 *****   ");
+                System.out.println("   *****   -------------------------------------------   *****   ");
+                System.out.println("   *****                    OTP ABOVE                    *****   ");
+                
+                return true;
+            } else {
+                System.out.println("This shouldn't happen. Contact devs pls!");
+                
+                return false;
+            }   
+        } else {
+            // user put wrong message returning to main
+            System.out.println("   *****   ___________________________________________   *****   ");
+            System.out.println("   *****                                                 *****   ");
+            System.out.println("   *****            ... returning to menu ...            *****   ");
+            return false;
+        }
+
+        }
+        
+
+
+       
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
     // creating processbuilder   
-    ProcessBuilder processBuilder = new ProcessBuilder();
+    // ProcessBuilder processBuilder = new ProcessBuilder();
 
     // Run a shell command
     // processBuilder.command("bash", "-c", "gpg --help");
@@ -17,24 +15,52 @@ public class Main {
     String command = "";
 
     // Tutorial
-    System.out.println("Welcome to the PGP-console-UI");
-    System.out.println("Write your commands in the Linux shell");
-    System.out.println("Follow the directions and write 'Q' to QUIT");
+    System.out.println("   ***********************************************************   ");
+    System.out.println("   ***********************************************************   ");
+    System.out.println("   *****                                                 *****   ");
+    System.out.println("   *****          Welcome to the PGP-console-UI          *****   ");
+    System.out.println("   *****   ___________________________________________   *****   ");
+    System.out.println("   *****      Write your commands in the Linux shell     *****   ");
+    System.out.println("   *****   Follow the directions and write 'Q' to QUIT   *****   ");
     
     while (running) {
-        System.out.println("... awaiting command ...");
+        // menu screen
+        System.out.println("   *****   ___________________________________________   *****   ");
+        System.out.println("   *****                                                 *****   ");
+        System.out.println("   *****   1 -> Decrypt password-less                    *****   ");
+        System.out.println("   *****   2 -> Advanced settings                        *****   ");
+        System.out.println("   *****   Q -> QUIT                                     *****   ");
+        System.out.println("   *****   ___________________________________________   *****   ");
+        System.out.println("   *****                                                 *****   ");
+        System.out.println("   *****              .. awaiting command ...            *****   ");
+
+        // checking user input
         command = sc.nextLine();
         if (command.toUpperCase().equals("Q")) {
-            System.out.println("... quitting ...");
+            System.out.println("   *****                  ... quitting ...               *****   ");
             break;
         }
-
         try {
-            processBuilder.command("bash", "-c", command);
+            switch (command) {
+                // Decrypting message
+                case "1":
+                Decryptz.decrypt();
+                break;
+                
+                case "2":
+                //TODO
+                    System.out.println("   *****            ... wrong input try again ...        *****   ");
+                    break;
+                default:
+                    break;
+            }
+
+
+            // processBuilder.command("bash", "-c", command);
 
     
             //Boiler plate initialization
-            Process process = processBuilder.start();
+            /* Process process = processBuilder.start();
             StringBuilder output = new StringBuilder();
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(process.getInputStream()));
@@ -52,7 +78,7 @@ public class Main {
                 System.out.println("Wrong command");
                 //abnormal...
             }
-    
+     */
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -60,6 +86,9 @@ public class Main {
         }
 
     }
+
+    // close the scanner
+    sc.close();
 
 
     }
