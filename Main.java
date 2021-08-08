7dfaa014474e0c7deea66a8f96c5d6edcacb85e3
Jonathan Kristensen <jonahank7@gmail.com>
2021-08-08 15:26:41 +0200

    Simple UI loop

--- Main.java
+++ Main.java
@@ -1,55 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        
+    // creating processbuilder   
     ProcessBuilder processBuilder = new ProcessBuilder();
 
-    // -- Linux --
-
     // Run a shell command
-    processBuilder.command("bash", "-c", "gpg --help");
+    // processBuilder.command("bash", "-c", "gpg --help");
+    Scanner sc = new Scanner(System.in);
+    boolean running = true;
+    String command = "";
 
-    // -- Windows --
-
-    // Run a command
-    // processBuilder.command("cmd.exe", "/c", "echo hello");
-
-    // Run a bat file
-    //processBuilder.command("C:\\Users\\mkyong\\hello.bat");
-
-    try {
-
-        Process process = processBuilder.start();
-
-        StringBuilder output = new StringBuilder();
-
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            output.append(line + "\n");
+    // Tutorial
+    System.out.println("Welcome to the PGP-console-UI");
+    System.out.println("Write your commands in the Linux shell");
+    System.out.println("Follow the directions and write 'Q' to QUIT");
+    
+    while (running) {
+        System.out.println("... awaiting command ...");
+        command = sc.nextLine();
+        if (command.toUpperCase().equals("Q")) {
+            System.out.println("... quitting ...");
+            break;
         }
 
-        int exitVal = process.waitFor();
-        if (exitVal == 0) {
-            System.out.println("Success!");
-            System.out.println(output);
-            System.exit(0);
-        } else {
-            //abnormal...
+        try {
+            processBuilder.command("bash", "-c", command);
+
+    
+            //Boiler plate initialization
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+    
+            //Creating output string
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+    
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println(output);
+            } else {
+                System.out.println("Wrong command");
+                //abnormal...
+            }
+    
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-    } catch (IOException e) {
-        e.printStackTrace();
-    } catch (InterruptedException e) {
-        e.printStackTrace();
     }
+
 
     }
 
