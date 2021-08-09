f87d8b258c3b3c4b9ff0a3e9a463dc631da0711f
Jonathan Kristensen <jonahank7@gmail.com>
2021-08-10 00:13:55 +0200

    Visuals changes, and displaying current user

--- Main.java
+++ Main.java
@@ -5,54 +5,64 @@ public class Main {
 
     public static void main(String[] args) {
 
-    // creating processbuilder   
-    // ProcessBuilder processBuilder = new ProcessBuilder();
-
-    // Run a shell command
-    // processBuilder.command("bash", "-c", "gpg --help");
     Scanner sc = new Scanner(System.in);
     boolean running = true;
     String command = "";
+    String currentUser = "[please set local user]";
 
     // Tutorial
     System.out.println("   ***********************************************************   ");
     System.out.println("   ***********************************************************   ");
     System.out.println("   *****                                                 *****   ");
     System.out.println("   *****          Welcome to the PGP-console-UI          *****   ");
-    System.out.println("   *****   ___________________________________________   *****   ");
+    System.out.println("   *****   -------------------------------------------   *****   ");
     System.out.println("   *****      Write your commands in the Linux shell     *****   ");
     System.out.println("   *****   Follow the directions and write 'Q' to QUIT   *****   ");
     
     while (running) {
         // menu screen
-        System.out.println("   *****   ___________________________________________   *****   ");
+        System.out.println("   *****   -------------------------------------------   *****   ");
+        System.out.println("           Current local user: " + currentUser + "      ");
+        System.out.println("   *****   -------------------------------------------   *****   ");
         System.out.println("   *****                                                 *****   ");
         System.out.println("   *****   1 -> Decrypt password-less                    *****   ");
-        System.out.println("   *****   2 -> Create new local key-pair                *****   ");
-        System.out.println("   *****   3 -> Advanced settings                        *****   ");
+        System.out.println("   *****   2 -> Set current local user                   *****   ");
+        System.out.println("   *****   3 -> Create new local key-pair                *****   ");
+        System.out.println("   *****   4 -> Advanced settings                        *****   ");
         System.out.println("   *****   Q -> QUIT                                     *****   ");
-        System.out.println("   *****   ___________________________________________   *****   ");
         System.out.println("   *****                                                 *****   ");
+        System.out.println("   *****   -------------------------------------------   *****   ");
         System.out.println("   *****              .. awaiting command ...            *****   ");
-
+        
         // checking user input
         command = sc.nextLine();
         if (command.toUpperCase().equals("Q")) {
-            System.out.println("   *****                  ... quitting ...               *****   ");
+            System.out.println("   *****   -------------------------------------------   *****   ");
+            System.out.println("   *****                                                 *****   ");
+            System.out.println("   *****                 ... quitting ...                *****   ");
+            System.out.println("   *****                                                 *****   ");
+            System.out.println("   *****   -------------------------------------------   *****   ");
             break;
         }
         try {
+            String tmp;
             switch (command) {
                 // Decrypting message
                 case "1":
                     Decryptz.decrypt();
                     break;
-                
+
                 case "2":
-                    Createz.create();
+                    tmp = SetUserz.setUser();
+                    currentUser = (tmp == null)?"[please set local user]" : tmp ;
+                    break;
+                
+                case "3":
+                    tmp = Createz.create();
+                    currentUser = (tmp == null)? "[please set local user]" : tmp;
                     break;
 
-                case "3":
+                case "4":
                     System.out.println("   *****             ... not implemented yet ...         *****   ");
                     break;
 
